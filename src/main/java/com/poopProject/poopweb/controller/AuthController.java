8168181d6b293f304319d0ad63155e4f3d1db9ce
Artur K <artur1kacsur@gmail.com>
2024-11-24 22:45:18 +0100

    added exceptions to html (email already + passwordToShort)

--- src/main/java/com/poopProject/poopweb/controller/AuthController.java
+++ src/main/java/com/poopProject/poopweb/controller/AuthController.java
@@ -3,10 +3,13 @@ package com.poopProject.poopweb.controller;
 import com.poopProject.poopweb.entity.User;
 import com.poopProject.poopweb.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @Controller
 @AllArgsConstructor
@@ -27,7 +30,15 @@ public class AuthController {
 
     @PostMapping("/register")
     public String register(@ModelAttribute("user") User user) {
-        userService.addUser(user);
-        return "redirect:/login";
+        if (user.getPassword().length() >= 6){
+            try {
+                userService.addUser(user);
+            }catch (DataIntegrityViolationException e){
+                return "redirect:/login?unknownEmail";
+            }
+            return "redirect:/login";
+        }else {
+            return "redirect:/login?passwordIsShort";
+        }
     }
 }
